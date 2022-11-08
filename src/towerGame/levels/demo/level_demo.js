a33eacef9ba66c29104933b8ed9c046d0adb2b5a
Jan Erik Skaiå Bisseth <jbi017@uit.no>
2022-11-08 20:30:06 +0100

    Some light settings

--- src/towerGame/levels/demo/level_demo.js
+++ src/towerGame/levels/demo/level_demo.js
@@ -8,13 +8,13 @@ export async function level_demo(scene_width, scene_length){
 
 	level_demo_build(scene_width, scene_length)
 
-	let pointLight1 = new THREE.PointLight(0xff0000, 0.8)
+	let pointLight1 = new THREE.PointLight(0xffffff, 0.5)
 	pointLight1.position.y = 40;
 	pointLight1.position.x = -50;
 	pointLight1.position.z = -30;
 	g_scene.add(pointLight1);
 
-	const pointLightFolder = g_lilGui.addFolder( 'Pointlight' );
+	const pointLightFolder = g_lilGui.addFolder( 'Demo level Pointlight' );
 	pointLightFolder.add(pointLight1, 'visible').name("On/Off");
 	pointLightFolder.add(pointLight1, 'intensity').min(0).max(1).step(0.01).name("Intensity");
 	pointLightFolder.addColor(pointLight1, 'color').name("Color");
