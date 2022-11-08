e9d6c778c6366f26ea00ad9f4698bd0cc6cc7572
Jan Erik Skaiå Bisseth <jbi017@uit.no>
2022-11-08 21:16:18 +0100

    Set some variables

--- src/towerGame/levels/demo/level_demo_build.js
+++ src/towerGame/levels/demo/level_demo_build.js
@@ -1,11 +1,11 @@
 
 import {createWall_no_windows} from "../../shapes/structures/walls/wall_no_windows.js";
 
-export async function level_demo_build(scene_width, scene_length){
+export async function level_demo_build(scene_width, scene_length, level_height){
 
-	await createWall_no_windows("wall_left", scene_width, scene_length, scene_width*0.01, {x:-scene_width/2, y:scene_width/2, z:0}, {x:0, z:0, y:Math.PI/2})
-	await createWall_no_windows("wall_back", scene_width, scene_length, scene_width*0.01, {x:0, y:scene_width/2, z:-scene_width/2}, {x:0, z:0, y:0})
-	await createWall_no_windows("wall_right", scene_width, scene_length, scene_width*0.01, {x:scene_width/2, y:scene_width/2, z:0}, {x:0, z:0, y:Math.PI/2})
-	await createWall_no_windows("wall_front", scene_width, scene_length, scene_width*0.01, {x:0, y:scene_width/2, z:scene_width/2}, {x:0, z:0, y:0}, 0.15)
+	await createWall_no_windows("wall_left", scene_width, level_height, scene_width*0.01, {x:-scene_width/2, y:level_height/2, z:0}, {x:0, z:0, y:Math.PI/2})
+	await createWall_no_windows("wall_back", scene_length, level_height, scene_width*0.01, {x:0, y:level_height/2, z:-scene_width/2}, {x:0, z:0, y:0})
+	await createWall_no_windows("wall_right", scene_width, level_height, scene_width*0.01, {x:scene_width/2, y:level_height/2, z:0}, {x:0, z:0, y:Math.PI/2})
+	await createWall_no_windows("wall_front", scene_length, level_height, scene_width*0.01, {x:0, y:level_height/2, z:scene_width/2}, {x:0, z:0, y:0}, 0.15)
 
 }
