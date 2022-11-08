042e423865ad10efe6bc4df03b1a8dc18a4aa462
Jan Erik Skaiå Bisseth <jbi017@uit.no>
2022-11-08 20:44:53 +0100

    Wall textures

--- src/towerGame/shapes/structures/walls/wall_transparent.js
+++ src/towerGame/shapes/structures/walls/wall_transparent.js
@@ -7,30 +7,14 @@ const COLLISION_GROUP_SPHERE = 2;
 const COLLISION_GROUP_MOVEABLE = 4;
 const COLLISION_GROUP_BOX = 8;       //..osv. legg til etter behov.
 
-export async function createWall_no_windows(name, width = 1, height = 1, depth = 1, position={x:0, y:0, z:0}, rotation={x:0, z:0, y:0}) {
+export function createWall_no_windows(name, width = 1, height = 1, depth = 1, position={x:0, y:0, z:0}, rotation={x:0, z:0, y:0}) {
 	const mass = 0; //Merk!
 	let color=0x00A6E5;
-
-	//*****
-	//* BasicMaterial med alphaMap (som kontrollerer gjennomsiktighet vha. en tekstur)
-	//*****
-	const loader = new THREE.TextureLoader();
-	const bricksTexture = await loader.loadAsync('../../../assets/textures/bricks2.jpg');
-	const alphamapTexture = await loader.loadAsync('../../../assets/textures/bricks2_alphamap.jpg');
-	const materialBasicAlpahamap = new THREE.MeshBasicMaterial({ map:bricksTexture, color: 0xffffff, wireframe:false, side: THREE.DoubleSide });
-	materialBasicAlpahamap.alphaMap = alphamapTexture;
-	materialBasicAlpahamap.transparent = true;
-
-	/****
-	 * MeshNormalMaterial
-	 * Fargen bestemmes av geometriens normalvektorer.
-	 */
-	const materialNormal = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide, wireframe:false, flatShading: false });
 
 	//THREE
 	let mesh = new THREE.Mesh(
 		new THREE.BoxGeometry(width,height,depth, 1, 1),
-		materialBasicAlpahamap);
+		new THREE.MeshStandardMaterial({color: color}));
 	mesh.name = 'name';
 	mesh.rotation.set(rotation.x, rotation.y, rotation.z);
 	mesh.castShadow = true;
