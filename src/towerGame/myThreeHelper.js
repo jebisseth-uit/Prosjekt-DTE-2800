2ed576cc31b65031ef585e8e10a791d6b93dfff0
Jan Erik Skaiå Bisseth <jbi017@uit.no>
2022-11-26 21:28:52 +0100

    Fixed declaration error

--- src/towerGame/myThreeHelper.js
+++ src/towerGame/myThreeHelper.js
@@ -161,10 +161,6 @@ export function handleKeys(delta, g_currentlyPressedKeys) {
 		}
 	}
 
-	let moveX =  moveDirection.right - moveDirection.left;
-	let moveZ =  moveDirection.back - moveDirection.forward;
-	let moveY =  0;
-
 	if (g_currentlyPressedKeys['KeyQ']) {
 		g_controls.reset();
 	}
